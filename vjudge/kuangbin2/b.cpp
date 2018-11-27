aea4402f136f585d4d391edbb2eb996ce5b4fac8
landcold7 <landcold7@gmail.com>
2018-11-27 09:32:58 +0800

    Fix MLE for eight problem

--- vjudge/kuangbin2/b.cpp
+++ vjudge/kuangbin2/b.cpp
@@ -17,7 +17,7 @@ using namespace std;
 #define fori(i, a, b) for (int i = (a); i < (b); ++i)
 #define ford(i, a, b) for (int i = (a); i > (b); --i)
 #define output(v) cout << (v) << '\n'
-#define codejam(ix, v) cout << "Case #" << (ix) << ": " << (v) << '\n'
+#define codejam(ix, v) cout << "Case " << (ix) << ": " << (v) << '\n'
 #define pvi(x, v) if(v) pvar(x); fora(a, x) cout << a << " "; pend
 #define par(x, n, v) if(v) pvar(x); fori(a, 0, n) cout << x[a] << " "; pend
 
@@ -53,14 +53,7 @@ typedef vector<vs> vvs;
 typedef pair<int, int> pii;
 typedef vector<pii> vpii;
 
-const int maxn = 500000;
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, 1, -1};
-char ops[] = {'d', 'u', 'l', 'r'};
 int facts[12] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800};
-int visit[maxn];
-string ss;
-
 int cantor(string& arg) {
   int ret = 1, len = sz(arg);
   fori (i, 0, len) {
@@ -94,71 +87,103 @@ typedef struct Stat {
   int pre;
   Stat(int v = 0, char o = 0, int p = 0) : val(v), op(o), pre(p) {}
 } Stat;
-Stat path[maxn], cur;
 
-void bfs() {
-  mst(visit, 0);
+const int maxn = 500000;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+char ops[][4] = {{'u', 'd', 'r', 'l'}, {'d', 'u', 'l', 'r'}};
+int visit[2][maxn];
+string ss, aa, bb;
+Stat path[maxn];
+set<int> st[2];
 
-  deque<Stat> dq;
-  string goal = "123456789";
-  Stat t(cantor(goal), '#', -1);
-
-  path[t.val] = t;
-  visit[t.val] = 1;
-  dq.pb(t);
-
-  while (sz(dq)) {
-    int l = sz(dq);
-    fori (i, 0, l) {
-      Stat cur = dq.front(); dq.pop_front();
-      string cs = inv_cantor(cur.val, 9);
-      int p = 0;
-      fori (i, 0, 9) if (cs[i] == '9') p = i;
-      int x = p / 3, y = p % 3;
-
-      fori (i, 0, 4) {
-        int nx = x + dx[i], ny = y + dy[i];
-        if (!(0 <= nx && nx < 3 && 0 <= ny && ny < 3)) continue;
-        int np = nx * 3 + ny;
-        string tt = cs;
-        swap(tt[p], tt[np]);
-
-        int hh = cantor(tt);
-        if (visit[hh]) continue;
-        visit[hh] = 1;
-        Stat nxt(hh, ops[i], cur.val);
-        path[hh] = nxt;
-        dq.pb(nxt);
-      }
-    }
+void strip(string& aa) {
+  fori (i, 0, sz(aa)) {
+    if (aa[i] == 'X') aa[i] = '9';
   }
 }
 
 void print_path() {
-  string bb = "", rr = "";
-  fori (i, 0, sz(ss)) {
-    if (ss[i] == 'x') bb += '9';
-    else if (ss[i] != ' ') bb += ss[i];
-  }
-
-  int kt = cantor(bb);
-
-  while (path[kt].op != '#' && path[kt].op) {
+  string rr = "";
+  int kt = cantor(aa);
+  trace(kt, path[kt].op, path[]);
+  while (path[kt].op) {
     rr += string(1, path[kt].op);
     kt = path[kt].pre;
   }
 
-  output(rr != "" ? rr : kt == 1 ? rr : "unsolvable");
+  kt = cantor(bb);
+  while (path[kt].op) {
+    rr += string(1, path[kt].op);
+    kt = path[kt].pre;
+  }
+  output(rr);
 }
 
-void solve() {
-  print_path();
+void bfs(int ii) {
+  mst(visit, 0);
+  mst(path, 0);
+
+  deque<Stat> dq[2];
+  Stat a(cantor(aa), '#', -1);
+  Stat b(cantor(bb), '#', - 1);
+
+  path[a.val] = a, path[b.val] = b;
+  visit[0][a.val] = 1, visit[1][b.val] = 1;
+  dq[0].pb(a), dq[1].pb(b);
+  st[0].insert(a.val), st[1].insert(b.val);
+
+  int level = 0;
+  while (sz(dq[0]) || sz(dq[1])) {
+
+    fori (i, 0, 2) {
+      // Check if two queues have the same state.
+      fora(v, st[0]) if (st[1].count(v)) {
+        codejam(ii, 2 * level + i + 1);
+        print_path();
+        return;
+      }
+      // pvi(st[i], 1);
+      st[i].clear();
+      //
+      int l = sz(dq[i]);
+      fori (j, 0, l) {
+        Stat cur = dq[i].front(); dq[i].pop_front();
+        string cs = inv_cantor(cur.val, 9);
+
+        int p = 0;
+        fori (t, 0, 9) if (cs[t] == '9') p = t;
+        int x = p / 3, y = p % 3;
+
+        fori (k, 0, 4) {
+          int nx = x + dx[k], ny = y + dy[k];
+          if (!(0 <= nx && nx < 3 && 0 <= ny && ny < 3)) continue;
+          int np = nx * 3 + ny;
+          string tt = cs;
+          swap(tt[p], tt[np]);
+
+          int hh = cantor(tt);
+          if (visit[i][hh]) continue;
+          visit[i][hh] = 1;
+
+          st[i].insert(hh);
+          Stat nxt(hh, ops[i][k], cur.val);
+          path[hh] = nxt;
+          dq[i].pb(nxt);
+        }
+      }
+    }
+    ++level;
+  }
 }
 
 int main() {
-  bfs();
-  while (getline(cin, ss)) {
-    solve();
+  int t; cin >> t;
+  fori (i, 1, t + 1) {
+    cin >> aa >> bb;
+    strip(aa), strip(bb);
+    trace(i, aa, bb);
+    bfs(i);
   }
   return 0;
 }
