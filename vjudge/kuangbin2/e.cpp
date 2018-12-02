5ae208f414bb9717605677e8e63b71be0c35f186
landcold7 <landcold7@gmail.com>
2018-12-02 17:38:31 +0800

    Add kuangbin2 e dfs solution

--- vjudge/kuangbin2/e.cpp
+++ vjudge/kuangbin2/e.cpp
@@ -21,18 +21,18 @@ using namespace std;
 #define pvi(x, v) if(v) pvar(x); fora(a, x) cout << a << " "; pend
 #define par(x, n, v) if(v) pvar(x); fori(a, 0, n) cout << x[a] << " "; pend
 
-// #define trace(...) _f(#__VA_ARGS__, __VA_ARGS__)
-// template <typename T>
-// void _f(const char* name, T&& arg) {
-//   cout << fixed << name << ": " << arg << '\n';
-// }
+#define trace(...) _f(#__VA_ARGS__, __VA_ARGS__)
+template <typename T>
+void _f(const char* name, T&& arg) {
+  cout << fixed << name << ": " << arg << '\n';
+}
 
-// template <typename T, typename... Args>
-// void _f(const char* names, T&& arg, Args&&... args) {
-//   const char* split = strchr(names + 1, ',');
-//   cout.write(names, split - names) << ": " << arg << " |";
-//   _f(split, args...);
-// }
+template <typename T, typename... Args>
+void _f(const char* names, T&& arg, Args&&... args) {
+  const char* split = strchr(names + 1, ',');
+  cout.write(names, split - names) << ": " << arg << " |";
+  _f(split, args...);
+}
 
 // double tick() {
 //   static clock_t old;
@@ -120,16 +120,85 @@ void solve() {
   ford (i, n - 1, -1) {
     finall = finall * shift + sz(seq[i]);
   }
+  // A optimization so that we dont need to call memset for every case.
   ++cnt;
   // trace(finall, shift);
   bfs();
+}
+
+
+int dfs(int depth, vi &pos, int &deep) {
+  // trace(depth, deep);
+  // pvi(pos, 1);
+  if (depth >= deep) {
+    return -1;
+  }
+
+  int diff = 0;
+  fori (i, 0, n) {
+    int tmp = sz(seq[i]) - pos[i];
+    diff = max(diff, tmp);
+  }
+
+  if (diff == 0) {
+    return depth;
+  }
+
+
+  if (depth + diff >= deep) {
+    return -1;
+  }
+
+  fori (i, 0, 4) {
+    vi npos = pos;
+    int ok = 0;
+    fori (j, 0, n) {
+      if (seq[j][pos[j]] == gene[i]) {
+        npos[j] += 1;
+        ok = 1;
+      }
+    }
+
+    if (ok) {
+      int ret = dfs(depth + 1, npos, deep);
+      if (ret != -1) {
+        return ret;
+      }
+    }
+  }
+
+  return -1;
+}
+
+void solve2() {
+  cin >> n;
+  seq.clear();
+  // mst(visit, 0);
+  int deep = 0;
+  fori (i, 0, n) {
+    string s; cin >> s;
+    seq.pb(s);
+    deep = max(sz(s), shift);
+  }
+
+  vi pos(n, 0);
+  while (1) {
+    int level = dfs(0, pos, deep);
+    if (level != -1) {
+      output(level);
+      return;
+    } else {
+      ++deep;
+    }
+  }
 }
 
 int main() {
   int t; cin >> t >> ws;
   cnt = 0;
   fori (i, 1, t + 1) {
-    solve();
+    // solve();
+    solve2();
   }
   return 0;
 }
