070b384ca2e098dfa4adc8b26bc66e26fc1a358d
landcold7 <landcold7@gmail.com>
2019-01-20 11:37:35 +0800

    New style for creating problem.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 mkdir -p ~/.local/bin/
-ln -sf $PWD/sketch.py      ~/.local/bin/sketch
-ln -sf $PWD/algo-run    ~/.local/bin/algo-run 
-ln -sf $PWD/algo-make   ~/.local/bin/algo-make 
-ln -sf $PWD/algo-split  ~/.local/bin/algo-split 
+ln -sf $PWD/sketch      ~/.local/bin/sketch
+ln -sf $PWD/algo-run    ~/.local/bin/algo-run
+ln -sf $PWD/algo-make   ~/.local/bin/algo-make
+ln -sf $PWD/algo-split  ~/.local/bin/algo-split
+
+cd third_party/ByteHelper
+npm install
+npm start
+
