4f0da6db18458897b3dd20856120539c8f762146
landcold7 <landcold7@gmail.com>
2019-04-07 18:22:37 +0800

    Add global-round-2

---
+++ codeforces/global-round-2/b/b.cpp
@@ -0,0 +1,77 @@
+//============================================================================
+// Name        : b
+// Date        : Sat Apr  6 19:48:01 CST 2019
+// Author      : landcold7
+// Description : Actions speak louder more than words
+//============================================================================
+#include "bits/stdc++.h"
+using namespace std;
+
+#define x first
+#define y second
+#define pb push_back
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define mst(x, y) memset(x, y, sizeof(x))
+#define pvar(x) cout << #x << ": "
+#define output(v) cout << (v) << '\n'
+#define fori(i, a, b) for (int i = a; i < b; ++i)
+#define jam(x, n) cout << "Case #" << x << ": " << n << "\n"
+#define prt(x, a, n) { cout << x[a]; if (a < n - 1) cout << " "; }
+#define par(x, s, n, v) if(v) pvar(x); fori(y, s, n) prt(x, y, n) cout << "\n"
+template<class T> inline void amin(T &x, const T &y) { if (y < x) x = y; }
+template<class T> inline void amax(T &x, const T &y) { if (x < y) x = y; }
+
+#ifndef _has_trace
+#define trace(...)
+#define dbstream ostream
+#endif
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+inline bool check(vi& a, int k, int h) {
+  vi b(a.begin(), a.begin() + k);
+  sort(rall(b));
+  for (int i = 0; i < k; i += 2) {
+    // int c = (i + 1 < k ? b[i + 1] : b[i]);
+    // h -= max(b[i], c);
+    h -= b[i];
+    if (h < 0) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void solve() {
+  int n, h;
+  cin >> n >> h;
+  vi a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  int lo = 1, hi = n;
+  while (hi - lo > 1) {
+    int mid = lo + (hi - lo) / 2;
+    if (check(a, mid, h)) {
+      lo = mid;
+    } else {
+      hi = mid;
+    }
+  }
+  output(check(a, hi, h) ? hi : lo);
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+  return 0;
+}
+
---
+++ codeforces/global-round-2/d/d.cpp
@@ -0,0 +1,74 @@
+//============================================================================
+// Name        : d
+// Date        : Sat Apr  6 20:47:59 CST 2019
+// Author      : landcold7
+// Description : Actions speak louder more than words
+//============================================================================
+#include "bits/stdc++.h"
+using namespace std;
+
+#define x first
+#define y second
+#define pb push_back
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define mst(x, y) memset(x, y, sizeof(x))
+#define pvar(x) cout << #x << ": "
+#define output(v) cout << (v) << '\n'
+#define fori(i, a, b) for (int i = a; i < b; ++i)
+#define jam(x, n) cout << "Case #" << x << ": " << n << "\n"
+#define prt(x, a, n) { cout << x[a]; if (a < n - 1) cout << " "; }
+#define par(x, s, n, v) if(v) pvar(x); fori(y, s, n) prt(x, y, n) cout << "\n"
+template<class T> inline void amin(T &x, const T &y) { if (y < x) x = y; }
+template<class T> inline void amax(T &x, const T &y) { if (x < y) x = y; }
+
+#ifndef _has_trace
+#define trace(...)
+#define dbstream ostream
+#endif
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+void solve() {
+  int n; cin >> n;
+  vl a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  sort(all(a));
+  vl d(n - 1);
+  for (int i = 0; i < n - 1; ++i) {
+    d[i] = a[i + 1] - a[i];
+  }
+  sort(all(d));
+  trace(a, d);
+  vl pre(n);
+  for (int i = 0; i < n - 1; ++i) {
+    pre[i + 1] = pre[i] + d[i];
+  }
+  trace(pre);
+  int m; cin >> m;
+  for (int i = 0; i < m; ++i) {
+    ll x, y;
+    cin >> x >> y;
+    ll w = y - x + 1;
+    int idx = lower_bound(all(d), w) - d.begin();
+    ll ret = (n - idx) * w + pre[idx];
+    trace(x, y, w, idx, n, ret);
+    cout << ret << (i == m - 1 ? "\n" : " ");
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+  return 0;
+}
+
