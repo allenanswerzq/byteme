15ad3ec84aac1e1d2748d7203b67b1264f72096c
landcold7 <landcold7@gmail.com>
2019-09-30 21:49:55 +0800

    Update codeforces downloader

--- codeforces/downloader.py
+++ codeforces/downloader.py
@@ -10,8 +10,8 @@ from urllib.request import urlopen
 MAX_SUBS = 1000000
 
 if (len(sys.argv) < 2):
-    print( 'Usage: python main.py <handle>')
-    exit(1)
+	print( 'Usage: python main.py <handle>')
+	exit(1)
 
 handle = sys.argv[1]
 
@@ -26,53 +26,57 @@ replacer = {'&quot;': '\"', '&gt;': '>', '&lt;': '<', '&amp;': '&', "&apos;": "'
 keys = replacer.keys()
 
 def get_ext(comp_lang):
-    if 'C++' in comp_lang:
-        return 'cpp'
-    for key in EXT_keys:
-        if key in comp_lang:
-            return EXT[key]
-    return ""
+	if 'C++' in comp_lang:
+		return 'cpp'
+	for key in EXT_keys:
+		if key in comp_lang:
+			return EXT[key]
+	return ""
 
 def parse(source_code):
-    for key in keys:
-        source_code = source_code.replace(key, replacer[key])
-    return source_code
+	for key in keys:
+		source_code = source_code.replace(key, replacer[key])
+	return source_code
 
 base_dir = DOWNLOAD_DIR + '/' + handle
 if not os.path.exists(base_dir):
-    os.makedirs(base_dir)
+	os.makedirs(base_dir)
 
 user_info_full_url = USER_INFO_URL.format(handle=handle, count=MAX_SUBS)
 print( 'Fetching user status: %s' % user_info_full_url)
 dic = json.loads(urlopen(user_info_full_url).read())
 if dic['status'] != u'OK':
-    print( 'Couldn\'t fetch user status')
-    exit(1)
+	print( 'Couldn\'t fetch user status')
+	exit(1)
 
 submissions = [submission for submission in dic['result'] if submission['verdict'] == u'OK']
 print( 'Fetching %d submissions' % len(submissions))
 
 start_time = time.time()
 for submission in submissions:
-    con_id, sub_id = submission['contestId'], submission['id'],
-    prob_name, prob_id = submission['problem']['name'], submission['problem']['index']
-    comp_lang = submission['programmingLanguage']
-    submission_full_url = SUBMISSION_URL.format(ContestId=con_id, SubmissionId=sub_id)
-    print( 'Fetching submission: %s' % submission_full_url)
-    submission_info = urlopen(submission_full_url).read()
-    soup = BeautifulSoup(submission_info, 'html.parser')
-    submission_text = soup.find('pre', id='program-source-text')
-    if submission_text is None:
-        print( 'Could not fectch solution %d', sub_id)
-        continue
-    result = submission_text.text.replace('\r', '')
-    ext = get_ext(comp_lang)
-    new_directory = base_dir + '/' + str(con_id)
-    if not os.path.exists(new_directory):
-        os.makedirs(new_directory)
-    file = open(new_directory + '/' + prob_id + ' [' + prob_name + ']' + '.' + ext, 'w')
-    file.write(result)
-    file.close()
+	try:
+		con_id, sub_id = submission['contestId'], submission['id'],
+		prob_name, prob_id = submission['problem']['name'], submission['problem']['index']
+		comp_lang = submission['programmingLanguage']
+		submission_full_url = SUBMISSION_URL.format(ContestId=con_id, SubmissionId=sub_id)
+		print( 'Fetching submission: %s' % submission_full_url)
+		submission_info = urlopen(submission_full_url).read()
+		soup = BeautifulSoup(submission_info, 'html.parser')
+		submission_text = soup.find('pre', id='program-source-text')
+		if submission_text is None:
+			print('Could not fectch solution %d', sub_id)
+			continue
+		result = submission_text.text.replace('\r', '')
+		ext = get_ext(comp_lang)
+		new_directory = base_dir + '/' + str(con_id)
+		if not os.path.exists(new_directory):
+			os.makedirs(new_directory)
+		file = open(new_directory + '/' + prob_id + ' [' + prob_name + ']' + '.' + ext, 'w')
+		file.write(result)
+		file.close()
+	except:
+		print('Catched an error, continue...')
+		continue
 end_time = time.time()
 
 duration_secs = int(end_time - start_time)
