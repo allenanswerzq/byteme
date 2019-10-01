738bbccb29e24e87e0578fe3c2f417b982cb41ee
landcold7 <landcold7@gmail.com>
2019-10-02 01:01:56 +0800

    Fix

--- setup.sh
+++ setup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-mkdir -p ~/.local/bin/
+mkdir -p $HOME/.local/bin/
 
-ln -sf $ALGO/sketch      ~/.local/bin/sketch
-ln -sf $ALGO/byte-run    ~/.local/bin/byte-run
-ln -sf $ALGO/byte-make   ~/.local/bin/byte-make
-ln -sf $ALGO/byte-split  ~/.local/bin/byte-split
-ln -sf $ALGO/clean       ~/.local/bin/byte-clean
+ln -sf "$ALGO/sketch"       "$HOME/.local/bin/sketch"
+ln -sf "$ALGO/byte-run"     "$HOME/.local/bin/byte-run"
+ln -sf "$ALGO/byte-make"    "$HOME/.local/bin/byte-make"
+ln -sf "$ALGO/byte-split"   "$HOME/.local/bin/byte-split"
 
-ln -sf $ALGO/sketch      ~/.local/bin/sketch
-ln -sf $ALGO/byte-run    ~/.local/bin/algo-run
-ln -sf $ALGO/byte-make   ~/.local/bin/algo-make
-ln -sf $ALGO/byte-split  ~/.local/bin/algo-split
-ln -sf $ALGO/clean       ~/.local/bin/algo-clean
+ln -sf "$ALGO/sketch"       "$HOME/.local/bin/sketch"
+ln -sf "$ALGO/byte-run"     "$HOME/.local/bin/algo-run"
+ln -sf "$ALGO/byte-make"    "$HOME/.local/bin/algo-make"
+ln -sf "$ALGO/byte-split"   "$HOME/.local/bin/algo-split"
 
 cd third_party/bytedump
 npm install
