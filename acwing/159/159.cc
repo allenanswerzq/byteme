1279bfc2492bed7d1aaa8cde111ed8b3dab4490e
landcold7 <landcold7@gmail.com>
2020-06-18 22:03:04 +0800

    Add more problems

---
+++ acwing/159/159.cc
@@ -0,0 +1,19 @@
+/* created   : 2020-06-18 07:56:56
+ * accepted  : 2020-06-18 07:56:58
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define x first
+#define y second
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+
+void solve() {
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+  return 0;
+}
