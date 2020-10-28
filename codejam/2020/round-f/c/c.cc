694575441ee842dc703978ca72be2282c5ef407b
landcold7 <landcold7@gmail.com>
2020-10-28 23:15:37 +0800

    Add more problems

---
+++ codejam/2020/round-f/c/c.cc
@@ -0,0 +1,20 @@
+/* created   : 2020-10-28 23:07:22
+ * accepted  : 2020-10-28 23:07:22
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+#define ll long long
+
+void solve() {
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0);
+  int tc; cin >> tc;
+  for (int i = 1; i <= tc; i++) {
+    cout << "Case #" << i << ": ";
+    solve();
+  }
+  return 0;
+}
