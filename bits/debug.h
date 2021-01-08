85c866f745936112356b926ebcb6a6396758894b
zqjxw73 <landcold7@gmail.com>
2021-01-08 23:42:14 +0800

    Update utils

--- bits/debug.h
+++ bits/debug.h
@@ -9,86 +9,99 @@ using namespace std;
 #define EXIT "\033[0m"
 #define CYAN "\033[0;36m"
 
-template <class T> struct rge { T b, e; };
-template <class T> rge<T> range(T i, T j) { return rge<T>{i, j}; }
-template <class T> char dud(...);
-template <class T> auto dud(T *x) -> decltype(cout << *x, 0);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
 
-template <typename T> struct is_vector : public std::false_type {};
+template <class T>
+struct rge {
+  T b, e;
+};
+template <class T>
+rge<T> range(T i, T j) {
+  return rge<T>{i, j};
+}
+template <class T>
+char dud(...);
+template <class T>
+auto dud(T *x) -> decltype(cout << *x, 0);
+
+template <typename T>
+struct is_vector : public std::false_type {};
 template <typename T, typename A>
 struct is_vector<std::vector<T, A>> : public std::true_type {};
 
-template <class T> static void __display_mat(vector<vector<T>> &v) {
+template <class T>
+static void __display_mat(vector<vector<T>> &v) {
   int n = v.size();
-  int m = v[0].size();
   int width = 0;
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      // string str = to_string(j) + "/" + to_string(v[i][j]);
+    for (int j = 0; j < (int)v[i].size(); j++) {
       string str = to_string(v[i][j]);
       width = max(width, (int)str.size());
     }
   }
-  fprintf(stderr, "\n");
+  cerr << "\n";
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      // string head = to_string(j);
-      string tail = to_string(v[i][j]);
-      // string str = head + "/";
-      // string str = head;
-      string str;
-      for (int x = 0 + tail.size() + 1; x < width; x++) {
-        str += " ";
+    for (int j = 0; j < (int)v[i].size(); j++) {
+      if (j > 0) {
+        cerr << ", ";
       }
-      str += tail;
-      fprintf(stderr, "%*s", width, str.c_str());
-      if (j != m - 1) {
-        fprintf(stderr, ", ");
-      }
+      cerr << setw(width) << v[i][j];
     }
-    fprintf(stderr, "\n");
+    cerr << "\n" << flush;
   }
+}
+
+template <class T>
+static void __display_mat(vector<T> &v) {
+  int n = (int) v.size();
+  int width = 0;
+  for (int j = 0; j < n; j++) {
+    string str = to_string(v[j]);
+    width = max(width, (int)str.size());
+  }
+  for (int j = 0; j < n; j++) {
+    if (j > 0) {
+      cerr << " ";
+    }
+    cerr << setw(width) << v[j];
+  }
+  cerr << flush;
 }
 
 struct debug {
   template <class T, class N>
-  typename enable_if<std::is_pointer<T>::value, debug &>::type
-  operator<<(std::tuple<T, N> &array) {
+  typename enable_if<std::is_pointer<T>::value, debug &>::type operator<<(
+      std::tuple<T, N> &array) {
     // Real place where printing out an array.
     auto mat = std::get<0>(array);
     int n = std::get<1>(array);
-    debug() << "[";
+    vector<int> ans(n);
     for (int i = 0; i < n; i++) {
-      if (i > 0) {
-        debug() << ", ";
-      }
-      debug() << mat[i];
+      ans[i] = mat[i];
     }
+    debug() << "[";
+    debug() << ans;
     debug() << "]";
     return *this;
   }
 
   template <class T, class N>
-  typename enable_if<is_vector<T>::value, debug &>::type
-  operator<<(std::tuple<T, N> &array) {
+  typename enable_if<is_vector<T>::value, debug &>::type operator<<(
+      std::tuple<T, N> &array) {
     // Real place where printing out a vector with custom size N.
     auto mat = std::get<0>(array);
     int n = std::get<1>(array);
     debug() << "[";
-    for (int i = 0; i < n; i++) {
-      if (i > 0) {
-        debug() << ", ";
-      }
-      // debug() << i << "/" << mat[i];
-      debug() << mat[i];
-    }
+    debug() << mat;
     debug() << "]";
     return *this;
   }
 
+
   template <class T, class H, class W>
-  typename enable_if<std::is_pointer<T>::value, debug &>::type
-  operator<<(std::tuple<T, H, W> &array) {
+  typename enable_if<std::is_pointer<T>::value, debug &>::type operator<<(
+      std::tuple<T, H, W> &array) {
     // Real place where printing out a two dimension array.
     auto mat = std::get<0>(array);
     int h = std::get<1>(array);
@@ -125,18 +138,43 @@ struct debug {
     return *this;
   }
 
-  template <class T> debug &operator<<(vector<vector<T>> &v) {
+  // template <class S, S (*op)(S, S), S (*e)()>
+  // debug &operator<<(Segtree<S, op, e> &seg) {
+  //   seg.debug();
+  //   return *this;
+  // }
+
+  template <class T>
+  typename enable_if<std::is_arithmetic<T>::value, debug &>::type &operator<<(vector<vector<T>> &v) {
     if (v.empty()) {
       return *this;
     }
-    int n = v.size();
-    debug() << "\n[";
-    for (int i = 0; i < n; i++) {
-      debug() << v[i];
-      if (i < n - 1) {
-        debug() << ",\n";
-      }
+    debug() << "[";
+    __display_mat<T>(v);
+    debug() << "]";
+    return *this;
+  }
+
+  template <class T>
+  typename enable_if<!std::is_arithmetic<T>::value, debug &>::type &operator<<(vector<vector<T>> &v) {
+    if (v.empty()) {
+      return *this;
     }
+    debug() << "[\n";
+    for (int i = 0; i < (int) v.size(); i++) {
+      debug() << v[i] << "\n";
+    }
+    debug() << "]";
+    return *this;
+  }
+
+  template <class T>
+   typename enable_if<std::is_arithmetic<T>::value, debug &>::type operator<<(vector<T> &v) {
+    if (v.empty()) {
+      return *this;
+    }
+    debug() << "[";
+    __display_mat<T>(v);
     debug() << "]";
     return *this;
   }
@@ -144,7 +182,10 @@ struct debug {
   template <class T>
   typename enable_if<sizeof(dud<T>(0)) != 1, debug &>::type operator<<(T i) {
     if (is_floating_point<T>::value) {
-      cerr << setprecision(20) << fixed << i << flush;
+      cerr << setprecision(10) << fixed << i << flush;
+    } else if (is_integral<T>::value) {
+      const int setw_size = 4;
+      cerr << setw(setw_size) << i << flush;
     } else {
       cerr << i << flush;
     }
@@ -156,24 +197,35 @@ struct debug {
     return *this << range(begin(i), end(i));
   }
 
-  template <class T, class b> debug &operator<<(pair<b, T> d) {
+  template <class T, class b>
+  debug &operator<<(pair<b, T> d) {
     return *this << "(" << d.first << ", " << d.second << ")";
   }
 
-  template <class T, class b> debug &operator<<(vector<pair<b, T>> v) {
+  template <class T, class b>
+  debug &operator<<(vector<pair<b, T>> v) {
     int n = v.size();
     *this << "[";
+    int width = 0;
+    for (int i = 0; i < n; i++) {
+      string s = to_string(v[i].first);
+      width = max(width, (int) s.size());
+      string t = to_string(v[i].second);
+      width = max(width, (int) t.size());
+    }
     for (int i = 0; i < n; i++) {
       if (i > 0) {
-        *this << ", ";
+        cerr << ", ";
       }
-      *this << "(" << v[i].first << ", " << v[i].second << ")";
+      cerr << "(" << setw(width) << v[i].first << ", "
+                  << setw(width) << v[i].second << ")";
     }
-    *this << "]";
+    cerr << "]" << flush;
     return *this;
   }
 
-  template <class T> debug &operator<<(rge<T> d) {
+  template <class T>
+  debug &operator<<(rge<T> d) {
     *this << "[";
     for (auto it = d.b; it != d.e; ++it) {
       if (it != d.b) {
@@ -185,11 +237,12 @@ struct debug {
   }
 };
 
-template <typename T> void __print(const string &names, T arg) {
+template <typename T>
+void __print(const string &names, T arg) {
   string name(names);
   if ((int)name.find("make_tuple") != -1 || (int)name.find("mt") != -1) {
-    int s = name.find('(');
-    int t = name.find(',');
+    int s = (int) name.find('(');
+    int t = (int) name.find(',');
     assert(t - s - 1 > 0);
     name = name.substr(s + 1, t - s - 1);
   }
@@ -206,18 +259,18 @@ template <typename T, typename... Args>
 void __print(const string &namexx, T arg, Args... args) {
   int p = -1;
   string names(namexx);
-  int n = names.size();
+  int n = (int)names.size();
   int x = (int)names.find('(');
   int y = (int)names.find(',');
   if (x != -1 && y != -1 && x < y) {
     // Looking for `()` pair first
     // handle calls like `trace(foo(a, b))`
-    p = names.find(')');
+    p = (int) names.find(')');
     assert(0 <= p && p < n);
-    p = names.find(',', p + 1);
+    p = (int) names.find(',', p + 1);
   } else {
     // Now looking for comma sign to split varibles.
-    p = names.find(',');
+    p = (int) names.find(',');
   }
   assert(p != -1);
   string name = names.substr(0, p);
@@ -225,8 +278,8 @@ void __print(const string &namexx, T arg, Args... args) {
     // Hacking to print out an array.
     // eg. trace(make_tuple(arr, w))
     // eg. trace(make_tuple(arr, h, w))
-    int s = name.find('(');
-    int t = name.find(',');
+    int s = (int) name.find('(');
+    int t = (int) name.find(',');
     assert(t - s - 1 > 0);
     name = name.substr(s + 1, t - s - 1);
   }
@@ -256,7 +309,7 @@ void __print(const string &namexx, T arg, Args... args) {
 template <typename T, typename... Args>
 void __trace(int ln, const string &fn, const string &var, T &&arg,
              Args &&... args) {
-  debug() << CYAN << "{" << fn << ":" << ln << "} " << EXIT;
+  cerr << CYAN << "{" << fn << ":" << setw(3) << ln << "} " << EXIT;
   __print(var, arg, args...);
 }
 
@@ -264,37 +317,22 @@ void __trace(int ln, const string &fn, const string &var, T &&arg,
 
 template <typename T>
 T &&__dbg(int ln, const string &fn, const string &expr, T &&val) {
-  debug() << CYAN << "{" << fn << ":" << ln << "} " << EXIT << expr << " = "
+  debug() << CYAN << "{" << fn << ":" << setw(3) << ln << "} " << EXIT << expr << " = "
           << val << '\n';
   return std::forward<T>(val);
 }
 
 #define debugstream debug
 
-#define xuyixuyi() __set_timer(__LINE__, __func__)
-void __set_timer(int line, const string &fn) {
-  static clock_t __start_time = -1;
-  static bool __start_flag = false;
-  static int __last_line = -1;
-  clock_t __curr_time = std::clock();
-  int __curr_line = line;
-  if (__start_flag) {
-    cerr << CYAN << "{xuyixuyi:" << fn << "("
-         << __last_line << "->" << __curr_line
-         << ")} " << EXIT << fixed << std::setprecision(2)
-         << 1000.0 * (__curr_time - __start_time) / CLOCKS_PER_SEC << " ms\n";
-  }
-  if (__start_flag == false) {
-    __start_flag = true;
-    __last_line = line;
-  }
-  __start_time = __curr_time;
-  __last_line = __curr_line;
+template <class T>
+void puts(T t) {
+  cout << t << "\n";
 }
-
-template<class T> void puts(T t) { cout << t << "\n"; }
-template<class T, class U, class... G>
-void puts(T t, U u, G... g) { cout << t << " "; puts(u, g...); }
+template <class T, class U, class... G>
+void puts(T t, U u, G... g) {
+  cout << t << " ";
+  puts(u, g...);
+}
 
 #define assertm(exp, msg) assert(((void)msg, exp))
 #define mt make_tuple
@@ -302,7 +340,7 @@ void puts(T t, U u, G... g) { cout << t << " "; puts(u, g...); }
 #else
 
 template <typename T>
-T&& identity(T&& t) {
+T &&identity(T &&t) {
   return std::forward<T>(t);
 }
 
@@ -313,45 +351,90 @@ T&& identity(T&& t) {
 
 // https://gcc.gnu.org/onlinedocs/gcc/Diagnostic-Pragmas.html
 // #pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wsign-compare"
+// #pragma GCC diagnostic ignored "-Wsign-compare"
 #pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+#define EXPECT_TRUE(a) assert(a)
+#define EXPECT_FALSE(a) assert(!a)
+#define EXPECT(a, b) assert(a == b)
 
 #include <graphviz/cgraph.h>
 #include <graphviz/gvc.h>
 
-class Graphviz {
-public:
-  explicit Graphviz(const string &name) : name_(name) { create_context(); }
+enum class GraphDrawType {
+  Directed,
+  Undirected,
+};
 
-  void add_edge(const string &u, const string &v, const string& label="") {
-    Agnode_t *n = agnode(gv_, (char*) u.c_str(), 1);
-    Agnode_t *m = agnode(gv_, (char*) v.c_str(), 1);
+class Graphviz {
+ public:
+  Graphviz() : Graphviz(GraphDrawType::Undirected, "g") {}
+
+  explicit Graphviz(GraphDrawType type) : Graphviz(type, "g") {}
+
+  Graphviz(GraphDrawType type, const string &name)
+      : gvc_(nullptr), gv_(nullptr), type_(type), name_(name) {
+    CreateContext();
+  }
+
+  ~Graphviz() { Clear(); }
+
+  void gvSetDirected() {
+    type_ = GraphDrawType::Directed;
+    Clear();
+    CreateContext();
+  }
+
+  void gvAddEdge(int a, int b) {
+    gvAddEdge(to_string(a), to_string(b), "");
+  }
+
+  void gvAddEdge(int a, int b, int c) {
+    gvAddEdge(to_string(a), to_string(b), to_string(c));
+  }
+
+  void gvAddEdge(const string &u, const string &v, const string &label) {
+    assert(++edge_cnt_ < 100);
+    Agnode_t *n = agnode(gv_, (char *)u.c_str(), 1);
+    Agnode_t *m = agnode(gv_, (char *)v.c_str(), 1);
     Agedge_t *e = agedge(gv_, n, m, 0, 1);
-    agsafeset(e, const_cast<char *>("label"), (char *) label.c_str(),
+    agsafeset(e, const_cast<char *>("label"), (char *)label.c_str(),
               const_cast<char *>(""));
   }
 
-  void draw_graph() {
+  void gvDrawGraph() {
     // Set layout engine.
     gvLayout(gvc_, gv_, "dot");
     name_ += ".pdf";
     gvRenderFilename(gvc_, gv_, "pdf", name_.c_str());
 
     gvFreeLayout(gvc_, gv_);
+  }
+
+ private:
+  void Clear() {
     agclose(gv_);
     gvFreeContext(gvc_);
   }
 
-private:
   // Set up a graphviz context.
-  void create_context() {
+  void CreateContext() {
     gvc_ = gvContext();
-    gv_ = agopen(const_cast<char *>("g"), Agdirected, 0);
+    if (type_ == GraphDrawType::Undirected) {
+      gv_ = agopen(const_cast<char *>("g"), Agundirected, 0);
+    } else {
+      gv_ = agopen(const_cast<char *>("g"), Agdirected, 0);
+    }
   }
 
+  int edge_cnt_ = 0;
   GVC_t *gvc_;
   Agraph_t *gv_;
+  GraphDrawType type_;
   std::string name_;
 };
 
-#endif // BYTEME_DEBUG_H_
+#pragma GCC diagnostic pop
+
+#endif  // BYTEME_DEBUG_H_
--- bits/stdc++.h
+++ bits/stdc++.h
@@ -1,6 +1,6 @@
 // C++ includes used for precompiling -*- C++ -*-
 
-// Copyright (C) 2003-2013 Free Software Foundation, Inc.
+// Copyright (C) 2003-2019 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
@@ -24,120 +24,122 @@
 
 /** @file stdc++.h
  *  This is an implementation file for a precompiled header.
-  */
+ */
 
-// Also writing "using namespace std;" here so that you dont need to write it everytime you start a cpp file
+// 17.4.1.2 Headers
 
-  // 17.4.1.2 Headers
+// C
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
 
-  // C
-  #ifndef _GLIBCXX_NO_ASSERT
-  #include <cassert>
-  #endif
-  #include <cctype>
-  #include <cerrno>
-  #include <cfloat>
-  #include <ciso646>
-  #include <climits>
-  #include <clocale>
-  #include <cmath>
-  #include <csetjmp>
-  #include <csignal>
-  #include <cstdarg>
-  #include <cstddef>
-  #include <cstdio>
-  #include <cstdlib>
-  #include <cstring>
-  #include <ctime>
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cuchar>
+#endif
 
-  #if __cplusplus >= 201103L
-  #include <ccomplex>
-  #include <cfenv>
-  #include <cinttypes>
-  #include <cstdbool>
-  #include <cstdint>
-  #include <ctgmath>
-  #include <cwchar>
-  #include <cwctype>
-  #endif
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
 
-  // C++
-  #include <algorithm>
-  #include <bitset>
-  #include <complex>
-  #include <deque>
-  #include <exception>
-  #include <fstream>
-  #include <functional>
-  #include <iomanip>
-  #include <ios>
-  #include <iosfwd>
-  #include <iostream>
-  #include <istream>
-  #include <iterator>
-  #include <limits>
-  #include <list>
-  #include <locale>
-  #include <map>
-  #include <memory>
-  #include <new>
-  #include <numeric>
-  #include <ostream>
-  #include <queue>
-  #include <set>
-  #include <sstream>
-  #include <stack>
-  #include <stdexcept>
-  #include <streambuf>
-  #include <string>
-  #include <typeinfo>
-  #include <utility>
-  #include <valarray>
-  #include <vector>
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
 
-  #if __cplusplus >= 201103L
-  #include <array>
-  #include <atomic>
-  #include <chrono>
-  #include <condition_variable>
-  #include <forward_list>
-  #include <future>
-  #include <initializer_list>
-  #include <mutex>
-  #include <random>
-  #include <ratio>
-  #include <regex>
-  #include <scoped_allocator>
-  #include <system_error>
-  #include <thread>
-  #include <tuple>
-  #include <typeindex>
-  #include <type_traits>
-  #include <unordered_map>
-  #include <unordered_set>
-  #endif
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#endif
 
-  // Hacking to add trace.
-  #include "debug.h"
+#if __cplusplus >= 201703L
+#include <any>
+#include <charconv>
+// #include <execution>
+#include <filesystem>
+#include <optional>
+#include <memory_resource>
+#include <string_view>
+#include <variant>
+#endif
 
-  // For the situation where oj doesn't support stdc++11 or above, so
-  // that i can quick copy this part.
-  #ifdef _common_headers
-  #include <vector>
-  #include <map>
-  #include <set>
-  #include <cmath>
-  #include <cstdio>
-  #include <queue>
-  #include <stack>
-  #include <string>
-  #include <limits>
-  #include <iomanip>
-  #include <fstream>
-  #include <sstream>
-  #include <cstdlib>
-  #include <utility>
-  #include <cstring>
-  #include <iostream>
-  #include <algorithm>
-  #endif
+#if __cplusplus > 201703L
+#include <bit>
+// #include <compare>
+// #include <span>
+// #include <syncstream>
+#include <version>
+#endif
+
+// Hacking to add trace.
+#include "debug.h"
