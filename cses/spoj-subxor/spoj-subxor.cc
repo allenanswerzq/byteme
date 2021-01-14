ea6daf3350d5cbb4928b244f604ae51482876b7b
zhangqiang <landcold7@gmail.com>
2021-01-14 20:19:47 +0800

    Update

--- cses/spoj-subxor/spoj-subxor.cc
+++ cses/spoj-subxor/spoj-subxor.cc
@@ -1,5 +1,5 @@
 /* created   : 2021-01-13 22:46:27
- * accepted  : $ACCEPTED
+ * accepted  : 2021-01-14 16:17:19
  */
 #include <bits/stdc++.h>
 using namespace std;
@@ -24,16 +24,16 @@ struct Trie {
   void add(int s) {
     int u = 0;
     auto bs = bitset<30>(s);
-    trace(bs);
+    trace(s, bs);
     for (int i = bs.size() - 1; i >= 0; i--) {
       int v = bs[i];
       // trace(i, v);
       assert(u < N);
       if (!node[u][v]) {
-        count[u]++;
         node[u][v] = p++;
       }
       u = node[u][v];
+      count[u]++;
     }
     leaf[u]++;
   }
@@ -42,20 +42,36 @@ struct Trie {
     int u = 0;
     auto bs = bitset<30>(s);
     auto bk = bitset<30>(K);
+    int ans = 0;
     for (int i = bs.size() - 1; i >= 0; i--) {
-      if (bs[i] < bk[i]) {
-        return count[u];
-      }
-      else if (bs[i] == bk[i]) {
-      }
-      else if (node[u][!c]) {
-        u = node[u][!c];
+      int c = bs[i], d = bk[i];
+      if (d) {
+        // 1): If the current bit of K is 1, `s` go to the same side,
+        // will have 0 which definitly less than k
+        // assert(node[u][c]);
+        if (node[u][c]) {
+          ans += count[node[u][c]];
+        }
+        // 2): go to the left side
+        if (node[u][!c]) {
+          u = node[u][!c];
+        }
+        else {
+          // assert(false);
+          return ans;
+        }
       }
       else {
-        assert(false);
+        if (node[u][c]) {
+          u = node[u][c];
+        }
+        else {
+          // assert(false);
+          return ans;
+        }
       }
     }
-    return 0;
+    return ans;
   }
 };
 
@@ -67,8 +83,10 @@ void solve() {
   trie.add(0);
   for (int i = 0; i < N; i++) {
     int x; cin >> x;
+    trace(i, x);
     s ^= x;
     ans += trie.find(s, K);
+    trace(i, ans);
     trie.add(s);
   }
   cout << ans << "\n";
