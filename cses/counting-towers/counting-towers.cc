75584f33358592025b45bf8f6d4527231b678156
zqjxw73 <landcold7@gmail.com>
2021-02-18 22:36:01 +0800

    Add more problems

---
+++ cses/counting-towers/counting-towers.cc
@@ -0,0 +1,19 @@
+/* created   : 2021-02-17 21:11:41
+ * accepted  : $ACCEPTED
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+
+void solve() {
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0);
+  int t; cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
