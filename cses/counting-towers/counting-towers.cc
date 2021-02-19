4dd238a813ec91c2a121fa14308830a31b3e5e01
zqjxw73 <landcold7@gmail.com>
2021-02-19 23:00:31 +0800

    Update

--- cses/counting-towers/counting-towers.cc
+++ cses/counting-towers/counting-towers.cc
@@ -6,7 +6,22 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 using ll = long long;
 
+const int mod = 1e9 + 7;
+
 void solve() {
+  int N; cin >> N;
+  vector<vector<ll>> f(N + 2, vector<ll>(2));
+  f[N + 1][0] = f[N + 1][1] = 1;
+  for (int i = N; i >= 2; i--) {
+    ll op1 = f[i + 1][0];
+    ll op2 = (f[i + 1][0] + f[i + 1][1]) % mod;
+    ll op3 = 2 * f[i + 1][0] % mod;
+    ll op4 = f[i + 1][1];
+
+    f[i][0] = (op1 + op2 + op3) % mod;
+    f[i][1] = (op1 + op4) % mod;
+  }
+  cout << (f[2][0] + f[2][1]) % mod << "\n";
 }
 
 int main() {
