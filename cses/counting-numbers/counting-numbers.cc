32e68abc5348644188896e742962dd7722b438c4
zqjxw73 <landcold7@gmail.com>
2021-02-28 22:43:21 +0800

    Update

--- cses/counting-numbers/counting-numbers.cc
+++ cses/counting-numbers/counting-numbers.cc
@@ -6,28 +6,42 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 using ll = long long;
 
-void brute_force(int A, int B) {
-  int cnt = 0;
-  for (int i = A; i <= B; i++) {
-    string s = to_string(i);
-    bool ok = true;
-    for (int j = 0; j + 1 < s.size(); j++) {
-      if (s[j] == s[j + 1]) {
-        ok = false;
-        break;
-      }
-    }
-    if (ok) {
-      cnt++;
-      trace(i);
-    }
+ll f[20][10][2][2];
+
+ll go(const string& num, int i, int x, bool leading_zero, bool tight) {
+  // NOTE: x is the digit should not be appeared at `i`th position
+  // trace(i, x, leading_zero, tight);
+  if (i == num.size()) {
+    return 1;
   }
-  trace(cnt);
+  if (~x && f[i][x][leading_zero][tight] != -1) {
+    return f[i][x][leading_zero][tight];
+  }
+  int lo = 0;
+  int hi = tight ? (num[i] - '0') : 9;
+  ll ans = 0;
+  for (int d = lo; d <= hi; d++) {
+    if (!leading_zero && d == x) {
+      continue;
+    }
+    ans += go(num, i + 1, d, leading_zero & d == 0, tight & d == hi);
+  }
+  if (~x) {
+    f[i][x][leading_zero][tight] = ans;
+  }
+  return ans;
+}
+
+ll compute(ll x) {
+  // 12345
+  memset(f, -1, sizeof(f));
+  string s = to_string(x);
+  return go(s, 0, -1, 1, 1);
 }
 
 void solve() {
-  int A, B; cin >> A >> B;
-  brute_force(1, 1212);
+  ll A, B; cin >> A >> B;
+  cout << compute(B) - compute(A - 1) << "\n";
 }
 
 int main() {
