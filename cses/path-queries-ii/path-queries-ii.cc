8c76ebb705665ab546fe54b95f6a95e265867747
zqxyz73 <zqxyz73@gmail.com>
2021-04-10 09:45:20 +0800

    Add more problems

--- cses/path-queries-ii/path-queries-ii.cc
+++ cses/path-queries-ii/path-queries-ii.cc
@@ -2,6 +2,7 @@
  * accepted  : $ACCEPTED
  */
 #include <bits/stdc++.h>
+#pragma GCC optimize("O2")
 using namespace std;
 #define all(x) (x).begin(), (x).end()
 using ll = long long;
@@ -31,8 +32,7 @@ struct Tree {
     depth.resize(n);
     heavy.resize(n, -1);
     head.resize(n);
-    // Do dfs
-    dfs(0);
+    pos.resize(n);
   }
 
   void add_edge(int u, int v) {
@@ -61,7 +61,7 @@ struct Tree {
 
   void decompose(int u, int h) {
     head[u] = h;
-    pos[v] = cnt++;
+    pos[u] = cnt++;
     if (heavy[u] != -1) {
       decompose(heavy[u], h);
     }
@@ -104,6 +104,7 @@ struct Tree {
     return (enter[u] <= enter[v] && leave[v] <= leave[u]);
   }
 
+  // Binary lifting
   int lca(int u, int v) {
     assert(0 <= u && u < n);
     assert(0 <= v && v < n);
@@ -124,7 +125,209 @@ struct Tree {
   }
 };
 
+struct Segtree {
+  struct node {
+    int mx = 0;
+
+    void apply(int l, int r, int val) { mx = val; }
+
+    string debug_string() { return to_string(mx); }
+  };
+
+  node unite(const node& a, const node& b) const {
+    return node{max(a.mx, b.mx)};
+  }
+
+  inline void push_down(int x, int l, int r) {
+    (void)x;
+    (void)l;
+    (void)r;
+  }
+
+  inline void pull_up(int x) {
+    tree[x] = unite(tree[x << 1], tree[x << 1 | 1]);
+  }
+
+  int n;
+  vector<node> tree;
+
+  void build(int x, int l, int r) {
+    if (l + 1 == r) {
+      return;
+    }
+    int y = l + (r - l) / 2;
+    build(x << 1, l, y);
+    build(x << 1 | 1, y, r);
+    pull_up(x);
+  }
+
+  template <typename U>
+  void build(int x, int l, int r, const vector<U>& v) {
+    if (l + 1 == r) {
+      assert(0 <= l && l < (int) v.size());
+      tree[x].apply(l, r, v[l]);
+      return;
+    }
+    int y = l + (r - l) / 2;
+    build(x << 1, l, y, v);
+    build(x << 1 | 1, y, r, v);
+    pull_up(x);
+  }
+
+  node query_impl(int x, int l, int r, int lx, int rx) {
+    if (rx <= l || r <= lx) {
+      return node{};
+    }
+    if (lx <= l && r <= rx) {
+      return tree[x];
+    }
+    push_down(x, l, r);
+    int y = l + (r - l) / 2;
+    node left = query_impl(x << 1, l, y, lx, rx);
+    node right = query_impl(x << 1 | 1, y, r, lx, rx);
+    node res = unite(left, right);
+    pull_up(x);
+    return res;
+  }
+
+  void debug_impl(int x, int l, int r, int lx, int rx, int level = 1) {
+    if (rx <= l || r <= lx) {
+      return;
+    }
+    if (l + 1 < r) {
+      push_down(x, l, r);
+    }
+    if (lx <= l && r <= rx) {
+      cerr << string(level, ' ')
+           << l << "->" << r << " ("
+           << tree[x].debug_string()
+           << ")\n";
+    }
+    if (l + 1 < r) {
+      int y = l + (r - l) / 2;
+      debug_impl(x << 1, l, y, lx, rx, 2 * level);
+      debug_impl(x << 1 | 1, y, r, lx, rx, 2 * level);
+      pull_up(x);
+    }
+  }
+
+  template <typename ...U>
+  void modify_impl(int x, int l, int r, int lx, int rx, const U&... v) {
+    if (rx <= l || r <= lx) {
+      return;
+    }
+    if (lx <= l && r <= rx) {
+      tree[x].apply(l, r, v...);
+      return;
+    }
+    int y = l + (r - l) / 2;
+    push_down(x, l, r);
+    if (lx < y) {
+      modify_impl(x << 1, l, y, lx, rx, v...);
+    }
+    if (rx > y) {
+      modify_impl(x << 1 | 1, y, r, lx, rx, v...);
+    }
+    pull_up(x);
+  }
+
+  int ceil_pow2(int x) {
+    int t = 1;
+    while (t < x) {
+      t <<= 1;
+    }
+    return t;
+  }
+
+  Segtree(int n_) : n(ceil_pow2(n_)) {
+    tree.resize(2 * n);
+    build(1, 0, n);
+  }
+
+  template <typename M>
+  Segtree(vector<M>& v) : n(ceil_pow2(v.size())) {
+    tree.resize(2 * n);
+    v.resize(n);
+    build(1, 0, n, v);
+  }
+
+  node query(int lx, int rx) {
+    assert(0 <= lx && lx <= rx && rx <= n);
+    return query_impl(1, 0, n, lx, rx);
+  }
+
+  template <typename ...M>
+  void modify(int lx, int rx, const M&... v) {
+    assert(0 <= lx && lx <= rx && rx <= n);
+    modify_impl(1, 0, n, lx, rx, v...);
+  }
+
+  void debug() {
+#ifdef LOCAL
+    debug_impl(1, 0, n, 0, n);
+#endif
+  }
+};
+
+int hld_query(Tree& tree, Segtree& seg, int a, int b) {
+  auto& pos = tree.pos;
+  auto& head = tree.head;
+  auto& depth = tree.depth;
+  auto& parent = tree.parent;
+  int res = 0;
+  for (; head[a] != head[b]; b = parent[head[b]]) {
+    if (depth[head[a]] > depth[head[b]]) {
+      swap(a, b);
+    }
+    res = max(res, seg.query(pos[head[b]], pos[b] + 1).mx);
+  }
+  if (depth[a] > depth[b]) {
+    swap(a, b);
+  }
+  res = max(res, seg.query(pos[a], pos[b] + 1).mx);
+  return res;
+}
+
+// TODO(TLE):
 void solve() {
+  int N, Q; cin >> N >> Q;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+  Tree t(N);
+  for (int i = 0; i < N - 1; i++) {
+    int u, v; cin >> u >> v;
+    u--, v--;
+    t.add_edge(u, v);
+    t.add_edge(v, u);
+  }
+  t.dfs(0);
+  t.build_lca();
+  t.build_hld();
+  Segtree seg(t.cnt);
+  for (int i = 0; i < N; i++) {
+    seg.modify(t.pos[i], t.pos[i] + 1, A[i]);
+  }
+  vector<int> ans;
+  for (int i = 0; i < Q; i++) {
+    int op; cin >> op;
+    if (op == 1) {
+      int s, x; cin >> s >> x;
+      s--;
+      seg.modify(t.pos[s], t.pos[s] + 1, x);
+    }
+    else {
+      int a, b; cin >> a >> b;
+      a--, b--;
+      int c = t.lca(a, b);
+      int r = max(hld_query(t, seg, a, c), hld_query(t, seg, b, c));
+      ans.push_back(r);
+    }
+  }
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << (i == ans.size() - 1 ? '\n' : ' ');
+  }
 }
 
 int main() {
