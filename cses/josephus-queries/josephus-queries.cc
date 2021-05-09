a8dbf0e5882baa0a53fc7ac19da87c43cf9f10fe
zqxyz73 <zqxyz73@gmail.com>
2021-05-09 15:46:07 +0800

    Update

--- cses/josephus-queries/josephus-queries.cc
+++ cses/josephus-queries/josephus-queries.cc
@@ -6,35 +6,25 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 using ll = long long;
 
-void solve() {
-  // 1 2 3 4 5 6 7 1 2 3 4 5 6 7
-  // 3 5 7
-  // 7 3
-  // 2 4 6 1 5 3 7
+int solve(int n, int k) {
+  if (n == 1) return 1;
 
-  // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
-  // 3 5 7 9 11 13 15 17
-  // 3 7 11 15
-  // 3 11
-  // 3
-  // 2 4 6 8 10 12 14 16 18 1 5 9 13 17
-
-  // 1 2 3 4 5 6
-  // 1 3 5
-  // 1 3
-  // 2 4 6 3 1 5
-
-  // 1 2 3 4 5
-  // 3 5
-  // 2 4 1 5 3
-  int N, K; cin >> N >> K;
+  if (k <= (n + 1) / 2) {
+    // 1 2 3 4 5 6 7
+    return (n & 1) ? (2 * k % n) : (2 * k);
+  }
+  else {
+    int c = solve(n >> 1, k - (n + 1) / 2);
+    return (n & 1) ? (2 * c + 1) : (2 * c - 1);
+  }
 }
 
 int main() {
   ios_base::sync_with_stdio(0), cin.tie(0);
   int t; cin >> t;
   while (t--) {
-    solve();
+    int N, K; cin >> N >> K;
+    cout << solve(N, K) << "\n";
   }
   return 0;
 }
