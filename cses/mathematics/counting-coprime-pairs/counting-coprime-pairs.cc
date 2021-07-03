eddc6116839d94636a75f377ea26b1a8bbe8ac67
zqxyz73 <zqxyz73@gmail.com>
2021-07-03 18:53:56 +0800

    Add more problems

--- cses/mathematics/counting-coprime-pairs/counting-coprime-pairs.cc
+++ cses/mathematics/counting-coprime-pairs/counting-coprime-pairs.cc
@@ -1,5 +1,5 @@
-/* created   : 2021-06-05 15:41:01
- * accepted  : 2021-06-05 20:22:38
+/* created   : 2021-06-27 17:11:38
+ * accepted  : 2021-07-03 18:53:24
  */
 #include <bits/stdc++.h>
 using namespace std;
@@ -7,25 +7,90 @@ using namespace std;
 using ll = long long;
 using ar = array<ll, 2>;
 
-template <class T>
-vector<int> prime_factor(T n) {
-  vector<int> fact;
-  for (T i = 2; i * i <= n; i++) {
-    if (n % i == 0) {
-      fact.push_back(i);
-      while (n % i == 0) {
-        n /= i;
+template <int N>
+vector<int> mobius() {
+  vector<int> pr;
+  vector<int> lp(N);
+  vector<int> ans(N);
+  ans[1] = 1;
+  for (int i = 2; i < N; i++) {
+    if (lp[i] == 0) {
+      // This number is a prime number.
+      lp[i] = i;
+      pr.push_back(i);
+      // Set to -1 as it only has odd number of primes
+      ans[i] = -1;
+    }
+    for (int j = 0; j < (int) pr.size() && pr[j] <= lp[i] && i * pr[j] < N; j++) {
+      int k = i * pr[j];
+      assert(k < N);
+      lp[k] = pr[j];
+      if (pr[j] == lp[i]) {
+        // i * pr[j] will have square primes
+        ans[k] = 0;
+      }
+      else {
+        ans[k] = -ans[i];
       }
     }
   }
-  if (n > 1) {
-    fact.push_back(n);
+  return ans;
+}
+
+const int Z = 1e6 + 7;
+
+// The number of ways chose 2 from n
+ll cnk(ll n) { return n * (n - 1) / 2; }
+
+void solve_mobius() {
+  int N; cin >> N;
+  vector<int> cnt(Z);
+  for (int i = 0; i < N; i++) {
+    int x; cin >> x;
+    cnt[x]++;
   }
-  return fact;
+  vector<int> mu = mobius<Z>();
+  ll ans = 0;
+  // From all paris minus the pairs that has gcd equal 2,3,4...
+  for (int i = 1; i < Z; i++) {
+    // Compute how many numbers can be divided by `i`
+    ll tot = 0;
+    for (int j = i; j < Z; j += i) {
+      tot += cnt[j];
+    }
+    // Inclusion-exclusion
+    // trace(i, mu[i], tot, cnk(tot));
+    ans += cnk(tot) * mu[i];
+  }
+  cout << ans << "\n";
 }
 
 void solve() {
   int N; cin >> N;
+  vector<int> cnt(Z);
+  for (int i = 0; i < N; i++) {
+    int x; cin >> x;
+    cnt[x]++;
+  }
+  // The number of pairs that has gcd exactly equal `i`
+  vector<ll> f(Z);
+  for (int i = 1; i < Z; i++) {
+    ll tot = 0;
+    for (int j = i; j < Z; j += i) {
+      tot += cnt[j];
+    }
+    // Now f[i] is the number of pairs theirs gcd is `i`, but not exactly
+    // Their gcd colud be i, 2*i, 3*i, 4*i
+    f[i] = cnk(tot);
+  }
+  trace(mt(f, 20));
+  for (int i = Z - 1; i >= 1; i--) {
+    // Make gcd for i exact by removing others
+    for (int j = i + i; j < Z; j += i) {
+      f[i] -= f[j];
+    }
+  }
+  cout << f[1] << "\n";
 }
 
 int main() {
